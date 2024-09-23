fcd25de8aa026b7f26983f3aadd4a17f7d26af76
arbitroy <ndauwa.austine20@students.dkut.ac.ke>
2024-09-23 14:08:59 +0300

    fixed a fatal mistake

--- app/head.tsx
+++ app/head.tsx
@@ -11,7 +11,7 @@ const Head = () => {
 
   const meta = {
     title: `Austine Ndauwa ${pathname === '/' ? '' : '· ' + titleFixedToShow}`,
-    description: `Hi! My name is Austine Ndauwa I'm from Venezuela and I have +3 year of work experience in JavaScript and React, I'm a lover of page design or other things and whenever I can I try to learn new technologies since I discovered programming at 16. I love what I do and I help others with what I can, I also use in web, mobile and desktop development Next.js, Angular, Ionic and Electron implementing good practices, clean architecture, pixel perfect and agile methodologies.`,
+    description: `Hi! My name is Austine Ndauwa I'm from Kenya and I have +3 year of work experience in JavaScript and React, I'm a lover of page design or other things and whenever I can I try to learn new technologies since I discovered programming at 16. I love what I do and I help others with what I can, I also use in web, mobile and desktop development Next.js, Angular, Ionic and Electron implementing good practices, clean architecture, pixel perfect and agile methodologies.`,
     keywords: 'Austine Ndauwa, Arbitroy, Austine Ndauwa portafolio, Arbitroy portafolio, Arbitroy portfolio, Austine Ndauwa portfolio, Arbitroy github, Austine Ndauwa github, Arbitroy LinkedIn, Austine Ndauwa LinkedIn, Web, Desarrollo web, Programador web, Diseño web, Paginas web, Aplicaciones web, Aplicaciones móviles, Aplicaciones escritorio, Desarrollo frontend, Programador frontend, HTML, CSS, Javascript, Typescript, React, Angular.',
     type: 'website'
   };
